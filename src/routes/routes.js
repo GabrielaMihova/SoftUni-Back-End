0df9f8109c533afccffe847f6ad832403ffb8901
Gabriela Mihova <gabriela.mihova210@gmail.com>
2024-02-13 18:58:15 +0200

    --

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,4 +8,12 @@ router.get("/", (req, res) => {
   res.render("home", { layout: false });
 });
 
+router.get("/about", (req, res) => {
+  res.render("about", { layout: false });
+});
+
+router.get("/create", (req, res) => {
+  res.render("create", { layout: false });
+});
+
 module.exports = router;

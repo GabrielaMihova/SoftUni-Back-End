5b02c723fdcc8f1bae0310c7735cd18af79bc378
GabrielaMihova <gabriela.mihova210@gmail.com>
2024-02-16 23:01:22 +0200

    Create movie controller and service

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
 
+const movieService = require("../services.movieService");
+
 router.get("/create", (req, res) => {
   res.render("create");
 });
 
 router.post("/create", (req, res) => {
-  console.log(req.body);
+  const newMovie = req.body;
+  movieService.create(newMovie);
   res.send("Movie should be created");
 });
 
